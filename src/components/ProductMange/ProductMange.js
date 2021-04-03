fcbc310f3e63b8bc1760f1a4fbbfcd79df6233bf
RakhuDey <rakhu_dey@yahoo.com>
2021-04-03 23:22:51 +0600

    add nav bar

--- src/components/ProductMange/ProductMange.js
+++ src/components/ProductMange/ProductMange.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import { Nav } from "react-bootstrap";
 import "./ProductMange.css";
 
 const ProductMange = () => {
@@ -22,37 +22,62 @@ const ProductMange = () => {
   };
 
   return (
-    <div>
-      <div className="row">
-        <div className="col">
-          {" "}
-          <strong> Name:</strong>
-        </div>
-        <div className="col">
-          <strong> Price:</strong>
-        </div>
-        <div class="col">
-          <strong> Weight:</strong>
-        </div>
-        <div class="col">
-          <strong> Action:</strong>
-        </div>
+    <div style={{}}>
+      <div class="row" style={{ float: "left" }}>
+        <Nav
+          className="col-md-12 d-none d-md-block bg-light sidebar"
+          activeKey="/home"
+          onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        >
+          <div className="sidebar-sticky"></div>
+          <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-1">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="link-2">Link</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+              Disabled
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
       </div>
-      {product.map((pd) => (
-        <div class="row">
-          <div class="col">{pd.name}</div>
-          <div class="col">{pd.price}</div>
-          <div class="col">{pd.weight}</div>
+      <div style={{ marginLeft: "100px" }}>
+        <div className="row">
+          <div className="col">
+            {" "}
+            <strong> Name:</strong>
+          </div>
+          <div className="col">
+            <strong> Price:</strong>
+          </div>
           <div class="col">
-            <button>
-              <i class="fas fa-edit"></i>
-            </button>
-            <button onClick={() => deleteProduct(pd._id)}>
-              <i class="fas fa-trash-alt"></i>
-            </button>
+            <strong> Weight:</strong>
+          </div>
+          <div class="col">
+            <strong> Action:</strong>
           </div>
         </div>
-      ))}
+        {product.map((pd) => (
+          <div class="row">
+            <div class="col">{pd.name}</div>
+            <div class="col">{pd.price}</div>
+            <div class="col">{pd.weight}</div>
+            <div class="col">
+              <button>
+                <i class="fas fa-edit"></i>
+              </button>
+              <button onClick={() => deleteProduct(pd._id)}>
+                <i class="fas fa-trash-alt"></i>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
